484a3f4e6be31dfb1c0e08d8355ff85af9eb97d5
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2025-01-12 22:43:58 -0500

    Remove old code

--- ios/BlurView.swift
+++ ios/BlurView.swift
@@ -44,28 +44,4 @@ class BlurView : UIView {
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  
-  // private func createBlurView(_ type: NSString) -> UIView {
-  //   let clampedBlur = clamp(blurIntensity, min: MIN_BLUR_INTENSITY, max: MAX_BLUR_INTENSITY)
-  //   let clampedSaturation = clamp(saturationIntensity, min: MIN_SATURATION_INTENSITY, max: MAX_SATURATION_INTENSITY)
-    
-  //   let style = UIBlurEffect.Style.from(string: type)
-  //   let isSystemMaterial = type.lowercased.contains("material")
-    
-  //   let blur = if type == PLAIN_BLUR_STYLE {
-  //     PlainBlurView(frame, clampedBlur, clampedSaturation)
-  //   } else if type == VARIABLE_BLUR_STYLE {
-  //     VariableBlurView(frame, clampedBlur, clampedSaturation, gradientPoints)
-  //   } else if isSystemMaterial {
-  //     SystemBlurView(frame, style)
-  //   } else {
-  //     RegularBlurView(frame, clampedBlur, clampedSaturation, style)
-  //   }
-    
-  //   blur.tag = VIEW_TAG
-  //   blur.frame = bounds
-  //   blur.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-    
-  //   return blur
-  // }
 }
